0ddc925190aa7c6d32a9ed2a9bdbff01227f98b4
smg0218 <128454773+smg0218@users.noreply.github.com>
2025-03-13 18:04:48 +0900

    0313 미션 테스트 뼈대 코드 파일 업로드

---
+++ MyAnswerTest.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.d_activity.mission;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MyAnswerTest {
+
+    @Test
+    public void DayCheck() {
+        int[] input = {1,3,5,8};
+        String value = MyAnswer.DayOfWeekPrint(input);
+
+        String result = "Monday, Wednesday, Friday, Unknown";
+        Assertions.assertEquals(result, value, "결과 값은 \"Monday, Wednesday, Friday, Unknown\" 여야 합니다.");
+    }
+}
